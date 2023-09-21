7aa7f8b6198f55b565dc02a9aa1a691643a5fffc
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-09-21 13:22:20 +0530

    Create pattern4.java

---
+++ Step1/1.2/pattern4.java
@@ -0,0 +1,21 @@
+/*
+
+N = 3
+
+1
+2 2 
+3 3 3
+
+*/
+
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+         for(int i=1; i<=n;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
