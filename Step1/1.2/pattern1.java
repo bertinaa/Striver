3d78a8862a27094e6831a1eb51f3e490fcc61058
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-09-21 13:17:57 +0530

    Create pattern1.java

---
+++ Step1/1.2/pattern1.java
@@ -0,0 +1,22 @@
+/*
+
+N=3
+* * * 
+* * *
+* * *
+
+*/
+
+public class Solution {
+    public static void nForest(int n) {
+        // Write your code here
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j < n; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    
+    }
+}
