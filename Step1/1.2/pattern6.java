0a9e83f9888054f4ffe3282696f18eb188b6a127
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-09-21 16:07:58 +0530

    Create pattern6.java

---
+++ Step1/1.2/pattern6.java
@@ -0,0 +1,20 @@
+/*
+
+N=3
+1 2 3
+1 2 
+1
+
+*/
+public class Solution {
+    public static void nNumberTriangle(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=n-(i-1);j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
